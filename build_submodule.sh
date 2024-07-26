27ef8b60f473229aa12e62fcca49e7ff52178482
loopedresolve <99568419+loopedresolve@users.noreply.github.com>
2024-07-25 21:26:08 -0400

    Update build_submodule.sh

--- build_submodule.sh
+++ build_submodule.sh
@@ -3,7 +3,7 @@
 source ".scripts/functions.sh"
 
 log::message "Updating smoldot-swift submodule"
-git submodule update --remote checkouts/smoldot-swift
+git submodule update --init --remote checkouts/smoldot-swift
 
 cd checkouts/smoldot-swift
 /bin/zsh "build_xcframework.sh" dev
